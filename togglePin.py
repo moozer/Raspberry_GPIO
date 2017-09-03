27f44fec6b65ba641e881f05316fd920579e490e
moz <moz@local>
2017-09-03 11:23:30 +0200

    refactor and added comments

--- togglePin.py
+++ togglePin.py
@@ -6,44 +6,60 @@ import time
 import argparse
 
 def handleCmdArgs():
+    ''' handles command line arguments
+    '''
     parser = argparse.ArgumentParser()
     parser.add_argument("pin", help="BCM pin number to toggle", type=int)
     parser.add_argument("--waittime", help="wait time in seconds between toggle", type=int, default=2)
-    
+
     args = parser.parse_args()
     return args
 
-def togglePin( pinNo, waitTime=2 ):
-    ''' pinNo: the BCM numbered pin to use
-        waitTime: the time in seconds between toggling
-        '''
-    
+def setupGpio( pinNo ):
+    ''' Sets parameters for the IOs
+    '''
+
     # set the pin numbering scheme
     GPIO.setmode(GPIO.BCM)
 
     # set pin for output
     GPIO.setup( pinNo, GPIO.OUT )
 
+def cleanupGpio():
+    ''' code to ensure that the IO ports are in a good state after
+        the program is run
+    '''
+    GPIO.cleanup()
+
+def togglePinLoop( pinNo )
+    # loop initial values
+    val = 1
+    count = 0
+
+    # loop forever
+    while (True):
+
+        print "%3d Turning pin %d %s"%(count, pinNo, "on" if val else "off" )
+        GPIO.output( pinNo, val)
+        time.sleep( waitTime )
+        val = 1 - val # toggle 0 and 1
+        count = count +1
+
+def togglePin( pinNo, waitTime=2 ):
+    ''' pinNo: the BCM numbered pin to use
+        waitTime: the time in seconds between toggling
+        '''
+
+    setupGpio( pinNo )
+
     # to catch ctrl+c in a nice way
     try:
-        # loop initial values
-        val = 1
-        count = 0
-
-        # loop forever
-        while (1):
-
-            print "%3d Turning pin %d %s"%(count, pinNo, "on" if val else "off" )
-            GPIO.output( pinNo, val)
-            time.sleep( waitTime )
-            val = 1 - val # toggle 0 and 1
-            count = count +1 
+        togglePinLoop( pinNo )
 
     except KeyboardInterrupt:
         print "cleanup"
 
-    GPIO.cleanup()
-
+    cleanupGpio()
 
 # run if this is the "main" program (as opposed to included as module)
 if __name__ == "__main__":
